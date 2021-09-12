e9f32b70d8ab4acafa9d5d1c3cc1cb3cb137f50f
aabarcam <andres.abarca@ug.uchile.cl>
2021-09-12 01:19:39 -0300

    portada addition

---
+++ js/navigation.js
@@ -0,0 +1,15 @@
+function goToPortada() {
+    location.href='portada.html';
+}
+
+function goToInformarEvento() {
+    location.href='informar_evento.html';
+}
+
+function goToListadoEventos() {
+    location.href='listado_de_eventos.html';
+}
+
+function goToEstadisticas() {
+    location.href='estadisticas.html';
+}
